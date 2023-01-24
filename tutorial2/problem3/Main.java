efcab43f03abdd5b9c460c13fa0a37e76f951bf2
Jerry Shum <30613366+JurryShrooms@users.noreply.github.com>
2023-01-24 10:30:30 -0500

    Removing sout

--- tutorial2/problem3/Main.java
+++ tutorial2/problem3/Main.java
@@ -14,7 +14,6 @@ public class Main {
 
     public static String reverseString(String str) {
         if (str.length() == 1) {
-            System.out.println("String is only 1 character long.");
             return str;
         } else {
             return reverseString(str.substring(1)) + str.charAt(0);
