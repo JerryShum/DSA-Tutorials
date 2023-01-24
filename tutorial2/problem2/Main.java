767b5f4f949b1d6afab99f92c66e74205dda0fba
Neh2332 <114631060+Neh2332@users.noreply.github.com>
2023-01-24 10:20:40 -0500

    Finished Problem 2
    1. First need to check if n is 1, if n is equal to 1 then multiply m times n, which leaves us with just m
    2. Next we would return m plus the function recursive(m, n-1)
    3. We subtract n by 1 until n is equal to 1, which ends the recursion and leads us with the answer of m

--- tutorial2/problem2/Main.java
+++ tutorial2/problem2/Main.java
@@ -2,5 +2,19 @@
 package tutorial2.problem2;
 
 public class Main {
+      // Recursion Method Function with M and N as parameters
+    public static int recursion(int m, int n) {
+        // Checks if N is equal to 1
+        if (n == 1)
+            return m;
+        else
+            // Return the value of M and N - 1 untill N is equal to 1
+            System.out.println(m + recursion(m, n - 1));
+        return m + recursion(m, n - 1);
+    }
     
+      public static void main(String[] args) throws Exception {
+        // Calling the recursion method with 3 and 4 as sample values for the parameters
+        System.out.println(recursion(3, 4));
+    }
 }
