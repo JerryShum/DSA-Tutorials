886428cc5cf980879145e8c6b72896704f75fec5
Jerry Shum <30613366+JurryShrooms@users.noreply.github.com>
2023-01-24 10:28:42 -0500

    Updating problem3

--- tutorial2/problem3/Main.java
+++ tutorial2/problem3/Main.java
@@ -9,10 +9,12 @@ public class Main {
         System.out.print("Enter a string: ");
         String str = scanner.nextLine();
         System.out.println("The reversed string is: " + reverseString(str));
+        scanner.close();
     }
 
     public static String reverseString(String str) {
         if (str.length() == 1) {
+            System.out.println("String is only 1 character long.");
             return str;
         } else {
             return reverseString(str.substring(1)) + str.charAt(0);
